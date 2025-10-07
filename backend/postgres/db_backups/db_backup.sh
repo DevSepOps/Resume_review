6d77e6b864dcc48c4883bc955c1fa4772dab8b65
sepehrmdn77 <sepehrmaadani98@gmail.com>
2025-10-07 13:38:13 +0300

    db backup and restore script and env file

---
+++ backend/postgres/db_backups/db_backup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DATE=$(date "+%F_%H-%M-%S")
+BACKUP_DIR=/devsepops/backend/postgres/db_backups/
+DB_CONTAINER=resume_postgres_prod
+DB_NAME="resume_db"
+DB_USER="user"
+DB_PASS="password"
+mkdir -p $BACKUP_DIR
+docker exec -e PGPASSWORD="$DB_PASS" $DB_CONTAINER pg_dump -U $DB_USER $DB_NAME > $BACKUP_DIR/backup_$DATE.sql
+find $BACKUP_DIR -mtime +7 -type f -delete
+
+# Do not publish this script and keep it safe
+# Use cronjobs for daily backups
+# Backup everyday at 03:00 AM
+# 0 3 * * * /devsepops/backend/postgres/db_backups//db_backup.sh
---
+++ backend/postgres/db_backups/db_restore.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BACKUP_FILE=/devsepops/backend/postgres/db_backups/backup_file.sql #backup-file
+DB_CONTAINER=resume_postgres_prod
+DB_NAME="resume_db"
+DB_USER="user"
+DB_PASS="password"
+mkdir -p $BACKUP_DIR
+docker exec -e PGPASSWORD="$DB_PASS" $DB_CONTAINER psql -U $DB_USER $DB_NAME < $BACKUP_FILE
